c508f97fa86efa3757c126e5cbc5cbd28e96a026
no0bAuntor <auntorruetete@gmail.com>
2025-09-01 00:32:41 +0600

    Remove navigation indicator text and arrows

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -338,20 +338,6 @@ export default function Home() {
             </button>
           </div>
 
-          {/* Navigation Indicator */}
-          <div className="mt-4 text-center">
-            <div className={`inline-flex items-center space-x-2 px-4 py-2 rounded-full text-sm ${
-              isDarkMode 
-                ? 'bg-gray-800/50 text-gray-400' 
-                : 'bg-gray-100/50 text-gray-500'
-            }`}>
-              <span>👈</span>
-              <span className="md:hidden">Swipe to navigate</span>
-              <span className="hidden md:inline">Click arrows to navigate</span>
-              <span>👉</span>
-            </div>
-          </div>
-
           {/* Word Dots Navigation */}
           <div className="flex justify-center mt-8 space-x-2 max-w-full overflow-x-auto pb-4">
             {words.map((_, index) => (
